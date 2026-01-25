bf1028a190634f0a66288017a6f28197327a7638
leyanzhi <leyanzhi@gmail.com>
2026-01-25 17:17:23 +0800

    new dataset

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ from utility import convert_key_types
 from postprocessing import postprocessing
 from bilevel_DE import run_parallel_de
 
-POP_SIZE        : int   = 16        # population size for DE (should be in multiples of <number of CPU cores> - 1)
+POP_SIZE        : int   = 32        # population size for DE (should be in multiples of <number of CPU cores> - 1)
 NUM_CORES       : int   = 4         # number of CPU cores used
 NUM_THREADS     : int   = 2         # number of threads used per candidate evaluation
                                     #   NUM_CORES * NUM_THREADS should be <= total available CPU cores
-MAX_ITER        : int   = 20         # maximum iterations for DE
+MAX_ITER        : int   = 40         # maximum iterations for DE
 DIFF_WEIGHT     : float = 0.7       # Differential Weight / Mutation: Controls jump size
                                     #   Higher = bigger jumps (exploration); Lower = fine-tuning (exploitation)
                                     #   Since population size is small, need to aggresively explore to avoid getting stuck.
@@ -26,7 +26,7 @@ CROSS_PROB      : float = 0.7       # Crossover Probability: How much DNA comes 
                                     #   0.7 means 70% of the genes change every step.
                                     #   We want to mix good genes quickly, so set it high.
 VAR_THRESHOLD   : float = 0         # variance threshold for early stopping of DE
-PENALTY_WEIGHT  : float = 0.5       # penalty weight for high price settings in leader fitness function
+PENALTY_WEIGHT  : float = 0.9       # penalty weight for high price settings in leader fitness function
 NUM_ANCHORS     : int   = 9         # number of anchors for DE
 VARS_PER_STEP   : int   = 3         # number of dimensions (variables) per time step (i.e. a_t, b_t, r_t)
 
