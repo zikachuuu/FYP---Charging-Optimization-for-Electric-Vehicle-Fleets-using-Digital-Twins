add0ad2075142ceeefb04f588d44c9712402d267
leyanzhi <leyanzhi@gmail.com>
2026-01-29 08:03:56 +0800

    up

--- config_DE.py
+++ config_DE.py
@@ -3,7 +3,7 @@ NUM_PROCESSES   : int   = 4         # number of independent processes for parall
                                     #   Should be <= total available CPU cores
 NUM_THREADS     : int   = 2         # number of threads used per process during DE evaluations
                                     #   NUM_PROCESSES * NUM_THREADS should be <= total available CPU cores
-MAX_ITER        : int   = 2         # maximum iterations for DE
+MAX_ITER        : int   = 20        # maximum iterations for DE
 DIFF_WEIGHT     : float = 0.7       # Differential Weight / Mutation: Controls jump size
                                     #   Higher = bigger jumps (exploration); Lower = fine-tuning (exploitation)
                                     #   Since population size is small, need to aggresively explore to avoid getting stuck.
@@ -11,11 +11,11 @@ CROSS_PROB      : float = 0.7       # Crossover Probability: How much DNA comes 
                                     #   0.7 means 70% of the genes change every step.
                                     #   We want to mix good genes quickly, so set it high.
 VAR_THRESHOLD   : float = 0         # variance threshold for early stopping of DE
-PENALTY_WEIGHT  : float = 0.1       # penalty weight for high price settings in leader fitness function
+PENALTY_WEIGHT  : float = 0.5       # penalty weight for high price settings in leader fitness function
 NUM_ANCHORS     : int   = 12         # number of anchors for DE (max is T-1)
 VARS_PER_STEP   : int   = 3         # number of dimensions (variables) per time step (i.e. a_t, b_t, r_t)
 
-RELAX_STAGE_2   : bool  = True      # whether to relax integrality constraints in follower model (Stage 2) during DE evaluations (for debugging)
+RELAX_STAGE_2   : bool  = False      # whether to relax integrality constraints in follower model (Stage 2) during DE evaluations (for debugging)
 
 """
 Notes for setting POP_SIZE and MAX_ITER:
